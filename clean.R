d5f2293e74a155bf13dea4c6683114dfcb5b70e7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:08:49 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,8 +1,8 @@
 
 cleanDataframe <- function()
 { 
-  dfStates <- read.csv("C://Sudhanshu//687//Week 3//scprc-est2017-18+pop-res.csv")
-  #dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"))
+  #dfStates <- read.csv("C:/Sudhanshu/687/Week 3/scprc-est2017-18+pop-res.csv")
+  dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"))
   dfStates <- dfStates[-1,]
   nrow(dfStates)
   dfStates <- dfStates[-52,]
