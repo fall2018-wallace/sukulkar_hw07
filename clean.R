a633c7bb6f3611eeface0d6ab262fbcc601842bf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:02:26 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,15 @@
 
-clean_data <- raw_data
+cleanDataframe <- function()
+{ 
+  #dfStates <- read.csv("C:/Sudhanshu/687/Week 3/scprc-est2017-18+pop-res.csv")
+  dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"))
+  dfStates <- dfStates[-1,]
+  nrow(dfStates)
+  dfStates <- dfStates[-52,]
+  dfStates <- dfStates[,-1:-4]
+  nrow(dfStates)
+  colnames(dfStates) <- c("stateName","population","popOver18","percentOver18")
+  return(dfStates)
+}
+
+dfClean <- cleanDataframe()
